500406c3e10b7338c1a3a092435002a355622c18
Nguyen Huu Manh <n-manh@eole.co.jp>
2018-05-10 16:29:34 +0900

    Initial Commit

---
+++ TranslationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TranslationDemo
+//
+//  Created by マイン・グエン・フー on 2018/05/10.
+//  Copyright © 2018年 マイン・グエン・フー. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
